e6cff44887d157927382adf14322b74fb6a7dda6
seymanurks <nurks1905@gmail.com>
2024-05-14 19:31:39 +0300

    created pages and components

---
+++ src/pages/Detail/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const Detail = () => {
+    return(
+        <SafeAreaView>
+            <Text>Details!</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Detail;
