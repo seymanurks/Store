62a0a77ce3722a997dc55d1b04c6ff9d20f5d7b3
Şeyma <100444600+seymanurks@users.noreply.github.com>
2024-05-18 20:31:36 +0300

    update Detail.js

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -1,11 +1,34 @@
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import styles from "./Detail.style"
+import Config from 'react-native-config';
+import useFetch from '../../hooks/useFetch';
+import Loading from '../../components/Loading';
+import Error from '../../components/Error';
 
-const Detail = () => {
+const Detail = ({route}) => {
+    const {id} = route.params
+    const {loading, error, data} = useFetch(`${Config.API_URL}/${id}`)
+
+    if (loading) {
+        <Loading/>
+    }
+
+    if (error) {
+        <Error/>
+    }
+
     return(
-        <SafeAreaView>
-            <Text>Details!</Text>
-        </SafeAreaView>
+        <View style = {styles.container}>
+            <View style = {styles.image_container}>
+              <Image source={{uri: data.image}} style = {styles.image} />
+            </View>
+            <View style = {styles.body_container}>
+                <Text style = {styles.title}>{data.title}</Text>
+                <Text style = {styles.description}>{data.description}</Text>
+                <Text style = {styles.price}>{data.price} €</Text>
+            </View>
+        </View>
     )
 }
 
